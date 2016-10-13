5ab92e6b4a2a6fe5fc1d5b3d3bba3651288804c8
dramanebamba <dramane.bamba@gmail.com>
2016-10-14 00:30:25 +0200

    installation

--- src/com/sdzee/servlets/Test.java
+++ src/com/sdzee/servlets/Test.java
@@ -1,6 +1,7 @@
 package com.sdzee.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,18 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Test extends HttpServlet {
 	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-		
+		response.setContentType("text/html");
+		response.setCharacterEncoding( "UTF-8" );
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<meta charset=\"utf-8\" />");
+		out.println("<title>Test</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>Ceci est une page générée depuis une servlet.</p>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 }
