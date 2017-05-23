d289567a35691864d12d66ec46f6c789f6a0e7a7
Théo Gelly <theo.gelly@gmail.com>
2017-05-23 21:59:32 +0200

    Exportation en fichier JSON fait Prochaine étape réaliser pages

--- src/pole_info/PersonneDAO.java
+++ src/pole_info/PersonneDAO.java
@@ -1,5 +1,11 @@
 package pole_info;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -9,14 +15,17 @@ import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
 import javax.transaction.Transactional;*/
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+
 
 @RequestScoped
 public class PersonneDAO {
 
 	private static final String QUERY_AUTH = "SELECT u FROM Personne u "
 			+ "WHERE u.email = :email AND u.password = :password";
-//	private static final String QUERY_ID = "SELECT u.id FROM Personne u "
-//			+ "WHERE u.email = :email AND u.password = :password";
+	//	private static final String QUERY_ID = "SELECT u.id FROM Personne u "
+	//			+ "WHERE u.email = :email AND u.password = :password";
 
 	private static final String PARAM_EMAIL = "email";
 	private static final String PARAM_PASSWORD = "password";
@@ -34,24 +43,57 @@ public class PersonneDAO {
 	}
 
 	public int getId(String email, String password){
-		
+
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pole");
 		EntityManager em = factory.createEntityManager();
 
 		Personne p = null;
-		
+
 		p = em.createQuery(QUERY_AUTH,Personne.class)
 				.setParameter(PARAM_EMAIL, email)
 				.setParameter(PARAM_PASSWORD, password)
 				.getSingleResult();
-		
+
 		em.close();
-		
+
 		return p.getId();
 	}
-	
+
+	@SuppressWarnings("resource")
+	public void exportJSON()
+	{
+		Gson json = new Gson();
+
+		List<String> affectations = new ArrayList<>();
+		affectations.add("Theo Gelly");
+		FileWriter writer;
+		
+		try 
+		{
+			File fil = new File("/Users/theogelly/Documents/test.json");
+			fil.createNewFile();
+			writer = new FileWriter(fil); 
+			
+			String json_string = json.toJson(affectations);
+			
+			System.out.println("Je veux export " + json_string + " dans le fichier : " + fil);
+			json.toJson(json_string, writer);
+			writer.flush();
+			writer.close();
+		} 
+		catch (JsonIOException e) 
+		{
+			throw new IllegalStateException(e);
+		} 
+		catch (IOException e) 
+		{
+			throw new IllegalStateException(e);
+		}
+	}
+
 	public boolean trouverPersonne(String email, String password){
 
+		exportJSON();
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pole");
 		EntityManager em = factory.createEntityManager();
 
@@ -65,11 +107,11 @@ public class PersonneDAO {
 				.getSingleResult();
 
 		System.out.println("PersDAO : " + p);
-		
+
 		System.out.println("query fin trouverPersonne");
-		
+
 		em.close();
-		
+
 		if(p != null){
 			return true;
 		}
@@ -77,5 +119,5 @@ public class PersonneDAO {
 			return false;
 		}
 	}
-	
+
 }
