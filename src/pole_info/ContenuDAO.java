f8a0dd715796125b6dbea5e678019be231bf05dd
dramanebamba <dramane.bamba@gmail.com>
2017-05-24 21:50:51 +0200

    contenuDAO maj

--- src/pole_info/ContenuDAO.java
+++ src/pole_info/ContenuDAO.java
@@ -1,13 +1,25 @@
 package pole_info;
 
 import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 import main.java.io.github.dramanebamba.pole_info.model.Contenu;
 
 @RequestScoped
 public class ContenuDAO {
-	
+
 	private static final String QUERY_CONT = "SELECT u FROM Contenu u ";
 	Contenu contenu;
+
+	public void creerContenu(Contenu contenu){
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("pole");
+		EntityManager em = factory.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(contenu);
+		em.getTransaction().commit();
+		em.close();
+	}
 
 }
