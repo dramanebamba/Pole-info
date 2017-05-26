99fc13b809dd8b0af8e8d6175e7d4f95b5b33ed8
Stanley Bt <staybt@gmail.com>
2017-05-26 18:23:57 +0200

    COMMIT

--- src/main/java/io/github/dramanebamba/pole_info/servlets/GetManageBackupServlet.java
+++ src/main/java/io/github/dramanebamba/pole_info/servlets/GetManageBackupServlet.java
@@ -150,7 +150,7 @@ public class GetManageBackupServlet extends HttpServlet {
 
 		RequestDispatcher dispatch = this.getServletContext().getRequestDispatcher("/WEB-INF/ListeBackup.jsp");
 		dispatch.forward(request, response);
-		//COMMIT NON FORWARD
+		//COMMIT NON FORWARD REJECT
 	}
 
 	/**
