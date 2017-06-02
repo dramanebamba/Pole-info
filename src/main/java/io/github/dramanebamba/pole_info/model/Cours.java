e74b3365da36253a2a07b7b0507868a606b538cd
Stanley Bt <staybt@gmail.com>
2017-06-02 17:03:23 +0200

    MODIFICATION ENTITY COURS - STB THG

--- src/main/java/io/github/dramanebamba/pole_info/model/Cours.java
+++ src/main/java/io/github/dramanebamba/pole_info/model/Cours.java
@@ -3,8 +3,6 @@ package main.java.io.github.dramanebamba.pole_info.model;
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,7 +12,6 @@ import javax.persistence.Table;
 public class Cours 
 {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id_contenu = 0;
 	
 	@Column(name = "id_master")
